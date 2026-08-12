e37933951677efc4f566c94f8b9c3ec4bc76dcae
Ehab <ehabdandoush2002@gmail.com>
2026-08-12 14:24:31 +0200

    Modifiziert von www.SourceFiles.app

---
+++ discord-bot/commands/warn.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder, MessageFlags } = require('discord.js');
+const { v4: uuidv4 } = require('uuid');
+const { safeReply } = require('../utils/helpers');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('warn')
+        .setDescription('Warn a user')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
+        .addUserOption(opt => opt.setName('user').setDescription('User').setRequired(true))
+        .addStringOption(opt => opt.setName('reason').setDescription('Reason').setRequired(true)),
+
+    async execute(interaction, client, db) {
+        const user = interaction.options.getUser('user');
+        const reason = interaction.options.getString('reason');
+        const warnId = uuidv4().split('-')[0];
+        const warning = { id: warnId, moderator: interaction.user.id, reason, timestamp: Date.now() };
+
+        const key = `warnings_${interaction.guild.id}_${user.id}`;
+        const warnings = await db.get(key) || [];
+        warnings.push(warning);
+        await db.set(key, warnings);
+
+        const embed = new EmbedBuilder()
+            .setColor('#00fbff') // Neon Blue
+            .setTitle('⚠️ User Warned')
+            .addFields(
+                { name: 'User', value: `${user.tag} (${user.id})`, inline: true },
+                { name: 'Moderator', value: `${interaction.user}`, inline: true },
+                { name: 'Reason', value: reason },
+                { name: 'Warning ID', value: `\`${warnId}\``, inline: true },
+                { name: 'Total Warnings', value: `${warnings.length}`, inline: true }
+            )
+            .setTimestamp();
+
+        await safeReply(interaction, { embeds: [embed] });
+    }
+};
