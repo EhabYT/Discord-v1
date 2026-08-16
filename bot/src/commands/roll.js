f7499b852bd9a53367797795fc185e0b97e3c80f
Ehab <ehabdandoush2002@gmail.com>
2026-08-16 07:35:37 +0200

    Modifiziert von www.SourceFiles.app

---
+++ bot/src/commands/roll.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('roll')
+        .setDescription('Roll a die!')
+        .addIntegerOption(opt => opt.setName('sides').setDescription('Number of sides (default 6)').setMinValue(2).setMaxValue(100)),
+
+    async execute(interaction, client, db) {
+        const sides = interaction.options.getInteger('sides') || 6;
+        const { safeReply } = client.helpers;
+
+        const result = Math.floor(Math.random() * sides) + 1;
+
+        const embed = new EmbedBuilder()
+            .setColor('#00FF88')
+            .setTitle('🎲 Dice Roll')
+            .setDescription(`You rolled a **${result}** (d${sides})!`)
+            .setTimestamp();
+
+        await safeReply(interaction, { embeds: [embed] });
+    }
+};
